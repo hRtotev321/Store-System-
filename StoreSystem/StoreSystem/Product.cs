8fc7e225ecc804a4b6f3c5d6ee865a444ea6a208
Dimitar-Dimitrovv <ml05tko@gmail.com>
2023-04-18 17:58:00 +0300

    Product.cs created

---
+++ StoreSystem/StoreSystem/Product.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+public abstract class Product
+{
+    private string name;
+    private int quantity;
+    private double deliverPrice;
+    private double percentageMarkup;
+
+    public Product(string name, int quantity, double deliverPrice, double percentageMarkup)
+    {
+        this.Name = name;
+        this.Quantity = quantity;
+        this.DeliverPrice = deliverPrice;
+        this.PercentageMarkup = percentageMarkup;
+    }
+
+    public string Name
+    {
+        get => this.name;
+        set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Product name must not be null or empty!");
+            
+
+            this.name = value;
+        }
+    }
+
+    public int Quantity
+    {
+        get => this.quantity;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Quantity cannot be less or equal to 0!");
+            }
+
+            this.quantity = value;
+        }
+    }
+
+    public double DeliverPrice
+    {
+        get => this.deliverPrice;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Deliver price cannot be less or equal to 0!");
+            }
+
+            this.deliverPrice = value;
+        }
+    }
+
+    public double PercentageMarkup
+    {
+        get => this.percentageMarkup;
+        set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Percentage markup cannot be less or equal to 0!");
+            }
+
+            this.percentageMarkup = value;
+        }
+    }
+
+    public double FinalPrice => this.DeliverPrice + (this.DeliverPrice * this.PercentageMarkup / 100);
+
+    public override string ToString()
+    {
+        return ($"Product: {this.Name} <{this.Quantity}>\n") +
+               ($"Deliver Price: {this.DeliverPrice:F2}\n") +
+               ($"Percentage Markup: {this.PercentageMarkup:F2}\n") +
+               ($"Final Price: {this.FinalPrice:F2}");
+    }
+}
